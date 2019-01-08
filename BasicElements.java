4048437e65597d826073370ed2386081ce1a5b1a
seleneartemis <reesenicolerivera@gmail.com>
2019-01-08 12:59:14 +0800

    jframe added

--- BasicElements.java
+++ BasicElements.java
@@ -3,6 +3,12 @@ import java.swing.JFrame;
   public class BasicElements{
 
   	public static void main(String args[]){
+  		JFrame myFrame = new JFrame();
+
+  		myFrame.setDefaultCloseOperation(myFrame);
+  		myFrame.setVisible(true);
+  		myFrame.setSize(900,600);
+
   		System.out.println("basic java elements");
 
   	}
